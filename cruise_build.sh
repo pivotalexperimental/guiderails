72b531fd4484819ca606276383ee8eef25e0c455
David Goudreau & Derrick Ko <pair+david+derrick@pivotallabs.com>
2010-11-08 12:29:33 -0800

    getting bundler installed for ci

--- cruise_build.sh
+++ cruise_build.sh
@@ -2,4 +2,13 @@
 
 source $HOME/.rvm/scripts/rvm && source .rvmrc
 
+# install bundler if necessary
+gem list --local bundler | grep bundler || gem install bundler || exit 1
+
+# debugging info
+echo USER=$USER && ruby --version && which ruby && which bundle
+
+# conditionally install project gems from Gemfile
+bundle check || bundle install || exit 1
+
 rake cruise
